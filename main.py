8e6d072208511dd3ca0d8de73d249f901dde5ad6
divyachandrapandi <divya4shivalaya@gmail.com>
2022-06-06 07:37:40 +0530

    updated POSTGRES

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from flask_gravatar import Gravatar
 from functools import wraps
 from flask import abort
 import smtplib
+import os
 
 
 
@@ -20,13 +21,13 @@ my_password = "xfnleymkhjcjmqkb"
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager = LoginManager(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",'sqlite:///blog.db')       #'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
